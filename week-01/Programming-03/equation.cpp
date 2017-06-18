3381a79bf9bd4c1c7d8d4b2c723251c71c72f17f
avtomato <avtomato@gmail.com>
2017-06-19 00:03:50 +0300

    Initial commit

---
+++ week-01/Programming-03/equation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+
+    double a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == 0) {
+        if (b != 0) {
+            cout << -1.*c/b;
+        }
+    } else {
+        double discriminant = b*b - 4*a*c;
+
+        if (discriminant > 0) {
+            cout << (-b + sqrt(discriminant))/(2*a) << " " << (-b - sqrt(discriminant))/(2*a);
+        } else if (discriminant == 0) {
+            cout << -1.*b/(2*a);
+        }
+    }
+
+    return 0;
+}
